06bfe887414dd956661ed43bd5dd3b1d915d4b12
Polly <147508946+porlowska@users.noreply.github.com>
2023-12-10 22:07:16 +0000

    updated comments

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 //changes colours of the input fields <textarea> depenfing on the hour.
-for (let i = 9; i <= 17; i++) {
+for (let i = 9; i <= 17; i++) { // each id of the text is equal to the hour of the event. 
     if (i < dayjs().hour()) {
-        $(`#${i}`).removeClass('present future').addClass('past');
+        $(`#${i}`).removeClass('present future').addClass('past'); // there are 3 css styles, it makes sure that past clasess are deleted so the styling does not colide. 
     } else if (i === dayjs().hour()) {
         $(`#${i}`).removeClass('past future').addClass('present');
     } else {
