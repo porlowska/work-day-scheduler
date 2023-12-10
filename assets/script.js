d926aa088ff16b517a91f333c326c9334e5c75a1
Polly <147508946+porlowska@users.noreply.github.com>
2023-12-10 18:29:38 +0000

    HTML code updated with layout of the planner

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// change colour of the backgorid past present fututre
+// target id of containers to check agains what hour is it
+// taget classes of the container which if statement to change colour 
+
+// change coilur of the button on hoover
+
+// save button to save to the local storage 
